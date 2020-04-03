bf0e3b0856ae2c2a14fa234e297082231dbf0d34
LIBINKAI <841116931@qq.com>
2020-04-04 05:52:51 +0800

    尝试一些其它的初始化方式

--- ann/bp/NeuralNetwork.py
+++ ann/bp/NeuralNetwork.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# 复现 https://github.com/Anonymoushhh/BP_neural_network 大佬的代码，并加上自己的注释
 class NeuralNetwork:
     def __init__(self, layer_num, learn_step, neuron_num_each_layer):
         # 网络层数，包括输出层
@@ -15,10 +16,13 @@ class NeuralNetwork:
         self.weight = []
         # numpy.random.normal函数有三个参数（loc, scale, size），分别代表生成的高斯分布的随机数的均值、方差以及输出的shape
         for i in range(layer_num):
-            self.weight.append(np.random.normal(
-                0.0, pow(self.neuron_num_each_layer[i + 1], -0.5),
-                (self.neuron_num_each_layer[i + 1], self.neuron_num_each_layer[i])
-            ))
+            # self.weight.append(np.random.normal(
+            #     0.0, pow(self.neuron_num_each_layer[i + 1], -0.5),
+            #     (self.neuron_num_each_layer[i + 1], self.neuron_num_each_layer[i])
+            # ))
+            # 生成[0,1)之间的数据
+            self.weight.append(np.random.random((self.neuron_num_each_layer[i + 1], self.neuron_num_each_layer[i])))
+            # self.weight.append(np.zeros((self.neuron_num_each_layer[i + 1], self.neuron_num_each_layer[i])))
 
     def update(self, train_x, train_y):
         inputs = np.array(train_x, ndmin=2).T
@@ -44,6 +48,7 @@ class NeuralNetwork:
                 self.output_errors.append(np.dot((self.weight[self.layer_num - i]).T, self.output_errors[i - 1]))
         # 反向传播
         for i in range(self.layer_num):
+            # f(x)* （1-f(x)）即为激活函数 f(x)的导函数，更新过程从后向前进行
             self.weight[self.layer_num - i - 1] += self.learn_step * np.dot(
                 (self.output_errors[i] * self.outputs[-1 - i] * (1.0 - self.outputs[-1 - i])),
                 np.transpose(self.outputs[-1 - i - 1]))
